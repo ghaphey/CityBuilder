092a96a86daf06b69350c0fc770c370a069daff2
Graham <graham.haphey@gmail.com>
2018-05-30 06:22:49 -0500

    implemented placement + rotation
    can dynamically place building at cursor location, as well as rotate. Snap to 1x1 grid by rounding
    TODO: collision between objects to prevent overlapping

--- Assets/Scripts/BuildElementUI.cs
+++ Assets/Scripts/BuildElementUI.cs
@@ -6,14 +6,48 @@ using UnityEngine.UI;
 public class BuildElementUI : MonoBehaviour {
 
     [SerializeField] GameObject house;
+    [SerializeField] Camera mainCamera;
 
-    
+    private bool movingObject;
+    private Vector3 rayCastTarget;
+    private GameObject newObject;
+    private bool invalidPlace = false;
+
+    private void Update()
+    {
+        if (movingObject)
+            ObjectPlace();
+    }
+
+
     public void HousePressed ()
     {
-        Instantiate(house, new Vector3(0.0f, 0.48f, 0.0f), Quaternion.identity);
-        // Now must figure out how to snap house to mouse, and don't give permanent position until
-        // mouse 1 clicked
+        newObject = Instantiate(house, new Vector3(0.0f, 0.45f, 0.0f), Quaternion.identity);
+        movingObject = true;
 
-        // still want to animate button
+      
     }
+
+    
+
+    void ObjectPlace ()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            newObject.transform.position = new Vector3(Mathf.Round(hit.point.x), newObject.transform.position.y, Mathf.Round(hit.point.z));
+
+        }
+
+        if (Input.GetMouseButtonDown(0) && !invalidPlace)
+            movingObject = false;
+        // MUST IMPLEMENT COLLISION DETECTION FOR BUILDING PLACEMENT
+        else if (Input.GetKeyDown("r"))
+        {
+            newObject.transform.Rotate(0.0f, 90.0f, 0.0f);
+        }
+    }
+
 }
---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    private int invalidPlacement = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //returns 0 if not colliding with any object
+    public int GetPlacement()
+    {
+        return invalidPlacement;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        invalidPlacement++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        invalidPlacement--;
+    }
+}
