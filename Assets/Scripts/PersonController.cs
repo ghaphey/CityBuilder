fac031a3d78926be3130332fa5721fd2949d93a8
Graham <graham.haphey@gmail.com>
2018-06-01 06:46:52 -0500

    Started building person AI

---
+++ Assets/Scripts/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PersonController : MonoBehaviour {
+
+    [SerializeField] Transform goal;
+
+    private NavMeshAgent agent;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FindStockpile()
+    {
+        GameObject[] list = GameObject.FindGameObjectsWithTag("Stockpile");
+        float distance = list[0].GetComponent<Transform>().;
+        float listIndex;
+        // NEED TO COMPARE DISTANCE "Vector3.Distance" and determine closest stockpile
+        for (int i = 1; i < list.Length; i++)
+        {
+
+        }
+    }
+}
