f6323c44c912db9fe60e4ffa5f669e41d020d615
Graham <graham.haphey@gmail.com>
2018-06-04 06:48:25 -0500

    added selecting + resource toggle for gathering

--- Assets/Scripts/BuildElementUI.cs
+++ Assets/Scripts/BuildElementUI.cs
@@ -8,9 +8,11 @@ public class BuildElementUI : MonoBehaviour {
 
     [SerializeField] GameObject house;
     [SerializeField] GameObject stockPile;
+    [SerializeField] GameObject selected;
+    [SerializeField] GameObject gatherToggle;
 
     private Camera mainCamera;
-    private Vector3 rayCastTarget;
+    private GameObject currentSelected = null;
 
     private void Start()
     {
@@ -19,8 +21,52 @@ public class BuildElementUI : MonoBehaviour {
 
     private void Update()
     {
+        MouseHandler();
+        ContextMenuController();
     }
 
+    private void MouseHandler()
+    {
+        if ( Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            // this function generates a particlesystem halo to denote whether an object is selected or not
+            // assigns the object clicked to currentSelected
+            if (Physics.Raycast(ray, out hit))
+            {
+                if ( hit.collider.gameObject.tag != "Ground")
+                {
+                    if (currentSelected != null)
+                        Destroy(currentSelected.transform.Find("Selected(Clone)").gameObject);
+                    GameObject newSelect = Instantiate(selected, hit.transform.position, Quaternion.Euler(-90.0f, 0.0f, 0.0f));
+                    newSelect.transform.parent = hit.transform;
+                    currentSelected = hit.collider.gameObject;
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destroy(currentSelected.transform.Find("Selected(Clone)").gameObject);
+            currentSelected = null;
+        }
+    }
+
+    private void ContextMenuController()
+    {
+        if (currentSelected != null)
+        {
+            if (currentSelected.transform.parent.name == "Resources")
+            {
+                // must fix toggle to have persistance for objects using "gather" tag
+                gatherToggle.SetActive(true);
+            }
+            else
+            {
+                gatherToggle.SetActive(false);
+            }
+        }
+    }
 
     public void HousePressed ()
     {
@@ -31,6 +77,13 @@ public class BuildElementUI : MonoBehaviour {
     {
         GameObject newStockpile = Instantiate(stockPile, new Vector3(0.0f, 0.001f, 0.0f), Quaternion.identity);
         ExecuteEvents.Execute<ICustomMessageTarget>(newStockpile, null, (x, y) => x.PlacingBuilding(mainCamera));
+    }
+    public void GatherToggeled()
+    {
+        if (currentSelected.tag != "Gather")
+            currentSelected.tag = "Gather";
+        else
+            currentSelected.tag = "Untagged";
     }
 
     // TODO: fix bug w/ interface when selecting between stockpile and house, sometimes creates stockpile instead
--- Assets/Scripts/PersonController.cs
+++ Assets/Scripts/PersonController.cs
@@ -5,30 +5,54 @@ using UnityEngine.AI;
 
 public class PersonController : MonoBehaviour {
 
-    [SerializeField] Transform goal;
+    private GameObject goal = null;
 
     private NavMeshAgent agent;
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        goal = FindClosestLocation("Gather");
+        if (goal != null)
+        {
+            print("moving");
+            agent.destination = goal.transform.position;
+        }
     }
 
-    void FindStockpile()
-    {
-        GameObject[] list = GameObject.FindGameObjectsWithTag("Stockpile");
-        float distance = list[0].GetComponent<Transform>().;
-        float listIndex;
-        // NEED TO COMPARE DISTANCE "Vector3.Distance" and determine closest stockpile
-        for (int i = 1; i < list.Length; i++)
-        {
+    // now must have person work on tree, then move resources to nearest stockpile
 
+    /// FindClosestLocation
+    ///
+    /// Finds the closest location via the "FindGameObjectsWithTag" function
+    /// location string must match a tag within unity for function to work
+    GameObject FindClosestLocation(string location)
+    {
+        float distance = Mathf.Infinity;
+        float curDistance = 0.0f;
+        GameObject closest = null;
+        GameObject[] list = GameObject.FindGameObjectsWithTag(location);
+        if (list.Length <= 0)
+        {
+            print("No " + location);
+            return null;
         }
+
+
+        // NEED TO COMPARE DISTANCE "Vector3.Distance" and determine closest stockpile
+        for (int i = 0; i < list.Length; i++)
+        {
+            curDistance = Vector3.Distance(transform.position, list[i].transform.position);
+            if (curDistance < distance)
+            {
+                distance = curDistance;
+                closest = list[i];
+            }
+        }
+        return closest;
     }
 }
