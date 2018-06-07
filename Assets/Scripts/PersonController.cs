aed9415225f3a202f32a545daa65c9b278189b9e
Graham <graham.haphey@gmail.com>
2018-06-07 06:47:56 -0500

    implemented stockpilebehavior, continued AI development

--- Assets/Scripts/PersonController.cs
+++ Assets/Scripts/PersonController.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PersonController : MonoBehaviour
 {
-    private enum personState { Idle, Moving, Working };
+    [SerializeField] float workDistance = 1.0f;
+    [SerializeField] int maxInventorySpace = 20;
+
+    private enum personState { Idle, MovingItem, Building, Moving, Working };
     private enum personTask { Unassigned, Worker, Builder};
 
     personState currState = personState.Idle;
@@ -13,9 +17,14 @@ public class PersonController : MonoBehaviour
 
     private GameObject goal = null;
     private NavMeshAgent agent;
+
+    private int workTimeLeft = 0;
+    private Inventory myInventory;
+
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = 1.0f;
     }
 
     // Update is called once per frame
@@ -24,17 +33,86 @@ public class PersonController : MonoBehaviour
         switch (task)
         {
             case personTask.Worker:
+                workerTask();
                 break;
             case personTask.Builder:
                 break;
             case personTask.Unassigned:
                 break;
         }
-        goal = FindClosestLocation("Gather");
-        if (goal != null)
+//        goal = FindClosestLocation("Gather");
+ //       if (goal != null)
+ //       {
+ //           currState = personState.Moving;
+ //           agent.destination = goal.transform.position;
+  //      }
+    }
+
+    private void workerTask()
+    {
+        switch(currState)
         {
+            case personState.Idle:
+                goal = FindClosestLocation("Gather");
+                if (goal != null)
+                {
+                    currState = personState.Moving;
+                    agent.destination = goal.transform.position;
+                    agent.isStopped = false;
+                }
+                else
+                    print("Nothing to Gather!");
+                break;
+            case personState.Moving:
+                if (goal == null)
+                {
+                    agent.isStopped = true;
+                    currState = personState.Idle;
+                }
+                else if (agent.remainingDistance <= workDistance)
+                {
+                    currState = personState.Working;
+                }
+                else if (agent.isStopped == true)
+                    agent.isStopped = false;
+                break;
+            case personState.Working:
+                PersonWorking();
+                break;
+            case personState.MovingItem:
+                if (goal == null)
+                {
+                    //find empty stockpile
+                    //if none exist, say so
+
+                }
+                break;
+            case personState.Building:
+                // not implemented
+                break;
+        }
+    }
+
+    private void PersonWorking()
+    {
+        //not finished
+        ResourceController resource = goal.GetComponent<ResourceController>();
+        if (resource == null)
+            print("can't gather not a resource");
+        if (workTimeLeft < resource.GetWorkTime())
+        {
+
+        }
+        else if (myInventory.num < maxInventorySpace)
+        {
+            goal = FindClosestLocation("Gather", myInventory.itemType);
             currState = personState.Moving;
-            agent.destination = goal.transform.position;
+            if (goal == null)
+                currState = personState.MovingItem;
+        }
+        else
+        {
+            currState = personState.MovingItem;
         }
     }
 
@@ -68,5 +146,44 @@ public class PersonController : MonoBehaviour
             }
         }
         return closest;
+    }
+
+    /// FindClosestLocation (based on resource)
+    ///
+    /// Finds the closest location via the "FindGameObjectsWithTag" function
+    /// location string must match a tag within unity for function to work
+    /// and must also match the desired resource type
+    GameObject FindClosestLocation(string location, string resourceType)
+    {
+        float distance = Mathf.Infinity;
+        float curDistance = 0.0f;
+        GameObject closest = null;
+        GameObject[] list = GameObject.FindGameObjectsWithTag(location);
+        if (list.Length <= 0)
+        {
+            print("No " + location);
+            return null;
+        }
+
+
+        // NEED TO COMPARE DISTANCE "Vector3.Distance" and determine closest stockpile
+        for (int i = 0; i < list.Length; i++)
+        {
+            curDistance = Vector3.Distance(transform.position, list[i].transform.position);
+            if (curDistance < distance)
+            {
+                // MUST DO SOMETHING HERE TO MAKE IT HAPPEN
+                distance = curDistance;
+                closest = list[i];
+            }
+        }
+        return closest;
+    }
+
+
+    private class Inventory
+    {
+        public int num = 0;
+        public string itemType = null;
     }
 }
--- Assets/Scripts/StockpileBehavior.cs
+++ Assets/Scripts/StockpileBehavior.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class StockpileBehavior : MonoBehaviour {
     [SerializeField] private int capacity = 100;
+    [SerializeField] private GameObject wood;
+    [SerializeField] private GameObject stone;
     private StockSlot[] currStock = new StockSlot[4];
 
     // want to designate 4 slots w/ capacity and keep track of what is here
@@ -14,11 +16,70 @@ public class StockpileBehavior : MonoBehaviour {
         {
             slot.number = 0;
             slot.rTag = null;
+            slot.item = null;
         }
 	}
-	
-	// Update is called once per frame
-	
+
+    // Update is called once per frame
+    private void Update()
+    {
+        int i = 0;
+        foreach (StockSlot slot in currStock)
+        {
+            i++;
+            if (slot.number == 0 && slot.item != null)
+            {
+                Destroy(slot.item);
+                slot.item = null;
+            }
+            else if (slot.number > 0 && slot.item == null)
+            {
+                switch(slot.rTag)
+                {
+                    case "Wood":
+                        slot.item = createItemAtSlot(i, wood);
+                        break;
+                    case "Stone":
+                        slot.item = createItemAtSlot(i, stone);
+                        break;
+                    default:
+                        print("Unknown item");
+                        break;
+
+                }
+            }
+        }
+    }
+
+    private GameObject createItemAtSlot( int index, GameObject item)
+    {
+        float x, z;
+        switch (index)
+        {
+            case 0:
+                x = 2.0f;
+                z = 2.0f;
+                break;
+            case 1:
+                x = 2.0f;
+                z = -2.0f;
+                break;
+            case 2:
+                x = -2.0f;
+                z = 2.0f;
+                break;
+            case 3:
+                x = -2.0f;
+                z = -2.0f;
+                break;
+            default:
+                x = 0.0f;
+                z = 0.0f;
+                break;
+        }
+        return Instantiate(item, new Vector3(x, 0.2f, z), Quaternion.identity, gameObject.transform);
+    }
+
     public int DepositItem (string itemTag, int amount)
     {
         int numLeft = amount;
@@ -26,12 +87,14 @@ public class StockpileBehavior : MonoBehaviour {
         {
             if (numLeft <= 0)
                 return 0;
-            if (slot.rTag == itemTag && slot.number < capacity)
+            if ( (slot.rTag == itemTag || slot.rTag == null) && slot.number < capacity)
             {
-                slot.number += amount;
+                slot.rTag = itemTag;
+                slot.number += numLeft;
                 if (slot.number > capacity)
                 {
                     numLeft = capacity - slot.number;
+                    slot.number -= numLeft;
                 }
                 else
                     return 0;
@@ -43,17 +106,38 @@ public class StockpileBehavior : MonoBehaviour {
 
     public int TakeItem (string itemTag, int amount)
     {
-        int numLeft = 0;
+        int total = 0;
+        int numLeft = amount;
         foreach (StockSlot slot in currStock)
         {
-            if (numLeft == amount)
-                return numLeft;
             if (itemTag == slot.rTag)
             {
-                numLeft = 
-                    // take the number we need, then adjust both to fix
+                // if number left can take from a slot without emptying, return running total + number left
+                if(slot.number - numLeft > 0)
+                {
+                    slot.number -= numLeft;
+                    return numLeft + total;
+                }
+                // if number left empties a slot perfectly, must set slot as empty
+                else if (slot.number - numLeft == 0)
+                {
+                    slot.number = 0;
+                    slot.rTag = null;
+                    return numLeft + total;
+                }
+                // if slot to negative, empty slot, reduce numleft and increase total
+                else
+                {
+                    total += slot.number;
+                    numLeft -= slot.number;
+                    slot.number = 0;
+                    slot.rTag = null;
+                }
+
             }
         }
+        // if we reach here, was not able to get enough resource for request, leave numleft only return running total
+        return total;
     }
 
     public bool QueryItem (string itemTag)
@@ -71,5 +155,6 @@ public class StockpileBehavior : MonoBehaviour {
     {
         public string rTag;
         public int number;
+        public GameObject item;
     }
 }
